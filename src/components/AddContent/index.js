9815ad70059488a3b49a2e064767139eb1199fc3
Jeff McAmmond <jeffmcammond@gmail.com>
2019-04-17 14:37:05 -0600

    spacing issues on github

--- src/components/AddContent/index.js
+++ src/components/AddContent/index.js
@@ -15,80 +15,80 @@ const CATEGORIES = [
 ];
 
 class AddContent extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { ...INITAL_STATE }
-	}
+    constructor(props) {
+        super(props);
+        this.state = { ...INITAL_STATE }
+    }
 
-	/**
-	 * When an input or select box is changed update the state
-	 */
-	onItemChange = event => {
-		this.setState({
-			[event.target.name]: event.target.value
-		});
-	}
+    /**
+     * When an input or select box is changed update the state
+     */
+    onItemChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
 
-	/**
-	 * When the user clicks the 'Add item' button, pass information to redux action and reset component state
-	 */
-	onAddItem = () => {
-		this.props.onAddItem({
-			name: this.state.name,
-			value: parseFloat(this.state.value).toFixed(2),
-			category: this.state.category
-		});
-		
-		this.setState({ ...INITAL_STATE });
-	}
+    /**
+     * When the user clicks the 'Add item' button, pass information to redux action and reset component state
+     */
+    onAddItem = () => {
+        this.props.onAddItem({
+            name: this.state.name,
+            value: parseFloat(this.state.value).toFixed(2),
+            category: this.state.category
+        });
+        
+        this.setState({ ...INITAL_STATE });
+    }
 
-	render() {
-		/**
-		 * Check is form is valid
-		 */
-		const isInvalid = 
-			this.state.name === '' ||
-			this.state.value === '' || 
-			this.state.category === '';
+    render() {
+        /**
+         * Check is form is valid
+         */
+        const isInvalid = 
+            this.state.name === '' ||
+            this.state.value === '' || 
+            this.state.category === '';
 
-		return (
-			<form className="add-content">
-				<h3>Add an item to the contents limit list</h3>
+        return (
+            <form className="add-content">
+                <h3>Add an item to the contents limit list</h3>
 
-				{/**
-				  * Name input
-				  */}
-				<label htmlFor="name">Name</label>
+                {/**
+                   * Name input
+                   */}
+                <label htmlFor="name">Name</label>
                 <input id="name" type="text" name="name" value={this.state.name} onChange={this.onItemChange}/>
 
-				{/**
-				  * Value input
-				  */}
-				<label htmlFor="value">Value</label>
-				<input id="value" type="number" name="value" value={this.state.value} onChange={this.onItemChange}/>
+                {/**
+                   * Value input
+                   */}
+                <label htmlFor="value">Value</label>
+                <input id="value" type="number" name="value" value={this.state.value} onChange={this.onItemChange}/>
 
-				{/**
-				  * Category select
-				  */}
-				<label htmlFor="category">Category</label>
-				<select id="category" value={this.state.category} name="category" onChange={this.onItemChange}>
-					<option value="" disabled>Select a category</option>
-					{CATEGORIES.map((cat, i) => {
-						return (
-							<option key={i} value={cat}>{cat}</option>
-						)
-					})}
-				</select>
+                {/**
+                   * Category select
+                   */}
+                <label htmlFor="category">Category</label>
+                <select id="category" value={this.state.category} name="category" onChange={this.onItemChange}>
+                    <option value="" disabled>Select a category</option>
+                    {CATEGORIES.map((cat, i) => {
+                        return (
+                            <option key={i} value={cat}>{cat}</option>
+                        )
+                    })}
+                </select>
 
-				{/**
-				  * Add item button
-				  */}
-				<button type="button" onClick={this.onAddItem} disabled={isInvalid}>
-					Add Item
-				</button>
-			</form>
-		)
-	}
+                {/**
+                   * Add item button
+                   */}
+                <button type="button" onClick={this.onAddItem} disabled={isInvalid}>
+                    Add Item
+                </button>
+            </form>
+        )
+    }
 }
 
 /**
